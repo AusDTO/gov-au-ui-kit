217ec9e76e5fe491462d11e417f5fb86caf7ce5e
Alexander Sadleir <maxious@lambdacomplex.org>
2016-06-16 17:07:40 +1000

    first revision of working js

--- assets/js/ui-kit.js
+++ assets/js/ui-kit.js
@@ -1,6 +1,9 @@
-$('.js-accordion-trigger').bind('click', function(e){
-    jQuery(this).parent().find('ul').slideToggle('fast');
-    // apply the toggle to the ul
-    jQuery(this).parent().toggleClass('is-expanded');
-    e.preventDefault();
+$( document ).ready(function() {
+    $('.js-accordion-trigger').bind('touchstart click', function (e) {
+        jQuery(this).parent().find('ul').slideToggle('fast');
+        jQuery(this).find(".chevron").toggleClass('top bottom');
+        // apply the toggle to the ul
+        jQuery(this).parent().toggleClass('is-expanded');
+        e.preventDefault();
+    });
 });
