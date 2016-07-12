e83cfd011b990c17807f1167bd442ee094bd4a0d
Petra Gulicher <petra@ustwo.com>
2016-07-12 12:38:58 +1000

    Add behaviour and styles to make Global Nav collapsible with JS

--- assets/js/ui-kit.js
+++ assets/js/ui-kit.js
@@ -1,3 +1,79 @@
+(function (document) {
+
+  /**
+   * Sets up the `html` element to be targeted in CSS
+   * @param {object} htmlElem - root DOMElement
+   */
+  function initJS(htmlElem) {
+    htmlElem.className = htmlElem.className.replace('no-js', 'js');
+  }
+
+  var CollapsibleNav = {
+
+    /**
+     * Initialise the Global Nav collapse behaviour
+     * @param {array} elems - list of collapsible elements
+     */
+    init: function(elems) {
+      for (var i = 0; i < elems.length; i++) {
+        this.initPanel(elems[i]);
+        this.initToggle(elems[i]);
+      }
+    },
+
+    /**
+     * Initialise the collapsible panel by setting its ID & 'aria-expanded' attributes
+     * @param {object} elem - the containing DOMElement
+     */
+    initPanel: function(elem) {
+      var panelLabel = elem.dataset.label || elem.className;
+
+      elem.id = panelLabel;
+      elem.setAttribute('aria-expanded', 'false');
+    },
+
+    /**
+     * Create a toggle element, attach an event listener and insert it into the DOM
+     * @param {object} elem - containing element for collapsible nav
+     */
+    initToggle: function(elem) {
+      var panelLabel = elem.dataset.label || elem.className,
+          toggle = document.createElement('button'),
+          self = this;
+
+      toggle.setAttribute('aria-controls', panelLabel);
+      toggle.className = panelLabel + '-toggle';
+      toggle.textContent = elem.dataset.toggleLabel || 'Menu';
+      toggle.targetElem = elem;
+      toggle.addEventListener('click', self.togglePanel);
+
+      elem.parentNode.insertBefore(toggle, elem);
+    },
+
+    /**
+     * Toggles ARIA attribute on the nav element
+     * @param {event} event - the event that triggered the toggle
+     */
+    togglePanel: function(event) {
+      var toggle = event.target,
+          elem = event.target.targetElem,
+          expanded = elem.getAttribute('aria-expanded') === 'true';
+
+      toggle.setAttribute('aria-expanded', expanded ? 'false' : 'true');
+      elem.setAttribute('aria-expanded', expanded ? 'false' : 'true');
+    }
+
+  };
+
+  // Kick of the JavaScript party when the DOM is ready
+  document.addEventListener('DOMContentLoaded', function() {
+    initJS(document.documentElement);
+    CollapsibleNav.init([document.getElementsByClassName('global-nav').item(0)]);
+  });
+
+})(document);
+
+
 $(document).ready(function () {
     $('.js-accordion-trigger').bind('touchstart click', function (e) {
 
