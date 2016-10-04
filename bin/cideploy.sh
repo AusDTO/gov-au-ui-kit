e1a6de4673f4ddcf0367f0fcd08cc0a40c9b0bc8
Alexander Sadleir <maxious@users.noreply.github.com>
2016-10-04 11:05:45 +1100

    Update cideploy.sh

--- bin/cideploy.sh
+++ bin/cideploy.sh
@@ -6,6 +6,6 @@ set -e
 curl -v -L -o cf-cli_amd64.deb 'https://cli.run.pivotal.io/stable?release=debian64&source=github'
 sudo dpkg -i cf-cli_amd64.deb
 cf -v
-cf login -a https://api.system.staging.digital.gov.au -o dto -u $CF_USER_STAGING -p $CF_PASSWORD_STAGING
+cf login -a https://api.system.staging.digital.gov.au -o dto -s dtomisc -u $CF_USER_STAGING -p $CF_PASSWORD_STAGING
 cf target -o dto -s dtomisc
 cf push gov-au-ui-kit$1 -b staticfile_buildpack -p ./build -i 1
