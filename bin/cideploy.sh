5150af737acec36871b7d167a48591bbc34f5c82
Alexander Sadleir <maxious@users.noreply.github.com>
2016-10-11 15:11:13 +1100

    Update cideploy.sh

--- bin/cideploy.sh
+++ bin/cideploy.sh
@@ -2,12 +2,10 @@
 
 # Exit immediately if any commands return non-zero
 set -e
-# Output the commands we run
-set -x
 # download cf-cli and use to push
 curl -v -L -o cf-cli_amd64.deb 'https://cli.run.pivotal.io/stable?release=debian64&source=github'
 sudo dpkg -i cf-cli_amd64.deb
 cf -v
-cf login -a https://api.system.staging.digital.gov.au -o dto -u $CF_USER_STAGING -p $CF_PASSWORD_STAGING
+cf login -a https://api.system.staging.digital.gov.au -o dto -s dtomisc -u $CF_USER_STAGING -p $CF_PASSWORD_STAGING
 cf target -o dto -s dtomisc
 cf push gov-au-ui-kit$1 -b staticfile_buildpack -p ./build -i 1
